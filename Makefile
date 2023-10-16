24b48269f1ef1d6bc2ddb49377c33494e53b3fd2
Kasim <qosimmax@gmail.com>
2023-10-16 11:38:51 +0500

    pubsub as proto

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 .PHONY: build install server test
 
 PROJECT_NAME=$(shell basename $(CURDIR))
+PROTO_PATH=$(CURDIR)/proto
 
 ## build: build the application
 build:
@@ -33,3 +34,19 @@ help:
 docker:
 	docker build -t $(PROJECT_NAME) .
 	docker run --rm --name $(PROJECT_NAME) -p 8000:8000 --env-file .env -it $(PROJECT_NAME)
+
+
+gen-proto:
+	protoc \
+    --go_out=./example/pb \
+    --go_opt=paths=import \
+    -I=$(PROTO_PATH)/fake-api \
+     $(PROTO_PATH)/fake-api/*.proto \
+
+
+
+add-submodule:
+	git submodule add git@github.com:qosimmax/services-proto.git ./proto
+
+pull-proto-module:
+	git submodule update --recursive --remote
--- server/internal/handler/example.go
+++ server/internal/handler/example.go
@@ -3,9 +3,11 @@ package handler
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"template-subscriber-go/example"
+	"template-subscriber-go/example/pb/fakeapi"
+
+	"google.golang.org/protobuf/proto"
 )
 
 // Example is an example event.
@@ -15,13 +17,19 @@ type Example struct {
 
 // Handle is the handler for the example event.
 func (e Example) Handle(ctx context.Context, data []byte) error {
-	var exampleData example.Data
-	err := json.Unmarshal(data, &exampleData)
+	fakeData := &fakeapi.FakeData{}
+	err := proto.Unmarshal(data, fakeData)
 	if err != nil {
 		return example.ErrNonRecoverable{
 			Err: fmt.Errorf("failed to unmarshal example data: %w", err),
 		}
 	}
+
+	exampleData := example.Data{
+		IsFake: fakeData.IsFake,
+		Date:   fakeData.GetDate().AsTime(),
+	}
+
 	// Do stuff here
 	err = e.DB.RecordExampleData(ctx, exampleData)
 	if err != nil {
